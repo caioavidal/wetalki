6268ecc76ff15ac5f3f99b3abbd3d87bc8cffe3f
Caio Augusto <caio.avidal@gmail.com>
2016-04-17 20:48:12 -0300

    added noty

--- public/app/dashboard/dashboardController.js
+++ public/app/dashboard/dashboardController.js
@@ -38,19 +38,64 @@ app.controller("DashboardController", function ($scope, lodash) {
     }
 
     socket.on("chat invitation sent", function (userName) {
-        console.log("Chat invitation sent to " + userName);
+        //console.log("Chat invitation sent to " + userName);
+        noty({
+            timeout: 5,
+            type: 'information',
+            text: 'Chat invitation sent to ' + userName,
+            theme: 'relax',
+            layout: 'topRight',
+            animation: {
+                open: 'animated flipInX', // Animate.css class names
+                close: 'animated flipOutX', // Animate.css class names
+            }
+        });
     })
     socket.on("invited to a chat", function (userWhoInvited) {
-        var anwser = confirm(userWhoInvited.name + " has invited you to a private text chat, do you want to accept?");
-        if (anwser === true) {
-            socket.emit("accepted chat invitation", userWhoInvited.socketId);
 
-            vm.isTextChatBoxVisible = true;
-            vm.peopleIAmChatting.push(userWhoInvited);
-        } else {
-            socket.emit("recused chat invitation", userWhoInvited.socketId);
-        }
-        $scope.$apply();
+        noty({
+            text: userWhoInvited.name + " has invited you to a private text chat, do you want to accept?",
+            theme: 'relax',
+            layout: 'topRight',
+            animation: {
+                open: 'animated flipInX', // Animate.css class names
+                close: 'animated flipOutX', // Animate.css class names
+            },
+            buttons: [
+                {
+                    addClass: 'btn btn-primary', text: 'Yes, I do', onClick: function ($noty) {
+
+                        $noty.close();
+
+                        socket.emit("accepted chat invitation", userWhoInvited.socketId);
+                        vm.isTextChatBoxVisible = true;
+                        vm.peopleIAmChatting.push(userWhoInvited);
+                        $scope.$apply();
+
+                    }
+                },
+                {
+                    addClass: 'btn btn-danger', text: "No, I don't", onClick: function ($noty) {
+                        $noty.close();
+                        socket.emit("recused chat invitation", userWhoInvited.socketId);
+
+                    }
+                }
+            ]
+        });
+
+
+
+        // var anwser = confirm(userWhoInvited.name + " has invited you to a private text chat, do you want to accept?");
+        // if (anwser === true) {
+        //     socket.emit("accepted chat invitation", userWhoInvited.socketId);
+
+        //     vm.isTextChatBoxVisible = true;
+        //     vm.peopleIAmChatting.push(userWhoInvited);
+        // } else {
+        //     socket.emit("recused chat invitation", userWhoInvited.socketId);
+        // }
+        // $scope.$apply();
     });
 
     socket.on("accepted chat invitation", function (userInvited) {
@@ -63,7 +108,18 @@ app.controller("DashboardController", function ($scope, lodash) {
 
     socket.on("recused chat invitation", function (userInvited) {
         //abrir janela de conversação
-        alert(userInvited.name + " has recused your chat invitation");
+
+        noty({
+            timeout: 5,
+            type: 'error',
+            text: userInvited.name + " has recused your chat invitation",
+            theme: 'relax',
+            layout: 'topRight',
+            animation: {
+                open: 'animated flipInX', // Animate.css class names
+                close: 'animated flipOutX', // Animate.css class names
+            }
+        });
     });
 
     vm.sendChatMessage = function (message, socketTargetId) {
